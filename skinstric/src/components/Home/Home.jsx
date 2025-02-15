afb2ea9b52c11070a1da7c856642a640685fd771
Oscar Campos <oscarc691@gmail.com>
2025-02-14 17:04:10 -0700

    still editing the styles sadly, all buttons function so my next target will be finsihing up the rest and styling after

--- skinstric/src/components/Home/Home.jsx
+++ skinstric/src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles.css";
+import { HiOutlinePaperAirplane } from "react-icons/hi2";
+
 
 const Home = () => {
   const navigate = useNavigate();
@@ -22,16 +24,20 @@ const Home = () => {
 
       {/* Side Navigation */}
       <div className="side-navigation left" onClick={() => navigate("/discover-ai")}>
-        <div className="nav-item">DISCOVER A.I.</div>
+        <div className="nav-item"><HiOutlinePaperAirplane className="leftArrow" />        DISCOVER A.I.</div>
       </div>
 
+      <div className="absolute right-0 top-1/2 -translate-y-1/2">
+      <div className="w-32 h-32 border-2 border-dotted border-gray-700 transform rotate-45 flex items-center justify-center">
       <div className="side-navigation right" onClick={() => navigate("/take-test")}>
-        <div className="nav-item">TAKE TEST</div>
+        <div className="nav-item">TAKE TEST         <HiOutlinePaperAirplane /></div>
+      </div>
+      </div>
       </div>
 
       {/* Footer */}
       <footer className="footer">
-        SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALISED ROUTINE TAILORED TO
+        SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALISED ROUTINE TAILORED TO WHAT YOUR SKIN NEEDS
       </footer>
     </div>
   );
